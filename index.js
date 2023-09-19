ea62a212712b0fcabd776eb1bb47f48ab5309ebe
Darius <dariusandriekus@gmail.com>
2023-09-19 10:48:07 +0300

    Comments

---
+++ index.js
@@ -0,0 +1,23 @@
+//Kintamieji - kur laikoma informacija
+//Palyginimas / salygos
+//Ciklai
+//Funkcijos
+//Ivykiai
+
+/*
+Kintamieji - kur laikoma informacija
+Palyginimas / salygos
+Ciklai
+Funkcijos
+Ivykiai
+/*
+
+
+// single line comment
+
+/* 
+    multi
+    line
+    comment
+/*
+
