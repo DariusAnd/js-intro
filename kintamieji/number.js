aef0392aaf1e0c1abeb82ff9e9340a82af0f236e
Darius <dariusandriekus@gmail.com>
2023-09-20 10:31:05 +0300

    Aritmetika ir nodemon install

--- kintamieji/number.js
+++ kintamieji/number.js
@@ -2,7 +2,7 @@
 NUMBER
 
 - teigiami / neigiami
-- sveikieji / desimtainiai
+- sveikieji / desimtainiai ~20 simboliu
 - normalus / nenormalus
 
 Inicijavimo budas:
@@ -15,12 +15,42 @@ const amzius = 99;
 let pinigine = 0;
 var minusas = -7.56;
 
+console.log(amzius);
+console.log(pinigine);
+console.log(minusas);
+
 const x1 = 1;
 const x2 = 2;
 const x3 = 3;
 const x4 = 4;
 const x5 = 5;
 
-console.log(amzius);
-console.log(pinigine);
-console.log(minusas);
+const n1 = 7;
+const n2 = 5;
+
+const sum = n1 + n2;
+console.log(sum);
+
+const diff = n1 - n2;
+console.log(diff);
+
+const multi = n1 * n2;
+console.log(multi);
+
+const div = n1 + n2;
+console.log(div);
+
+const complex = (2 + 2) *2
+console.log(complex)
+
+const exp1 = 2 + 2;
+const exp2 = 2 * 2;
+const exp3 = 2 ** 2;
+
+console.log(exp1, exp2, exp3);
+
+const exp4 = 2 ** 1;
+const exp5 = 2 ** 2;
+const exp6 = 2 ** 3;
+
+console.log(exp4, exp5, exp6);
