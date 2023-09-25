fec8d1a8831080754433e8f46b60df4c7d6f74c3
Darius <dariusandriekus@gmail.com>
2023-09-25 17:21:36 +0300

    let

--- kintamieji/array.js
+++ kintamieji/array.js
@@ -35,6 +35,8 @@ const pazymioPozicija = pazymiai.lenght -1;
 const paskutinisPazymys = pazymiai[pazymiai.length -1];
 console.log(paskutinisPazymys);
 
+console.clear();
+
 // Masyvas pazymiu:
 //Suskaiciuoti pazymiu vidurki ir isvesti sakiny:
 //'Studentu pazymiu vidurkis yra:'
@@ -47,10 +49,109 @@ const skaiciai = [9, 8, 6, 10, 8, 7];
 const suma = skaiciai[0] + skaiciai[1] + skaiciai[2] + skaiciai[3] + skaiciai[4] + skaiciai[5];
 const vidurkis = suma / skaiciai.length;
 
-console.log('Studentų pažymių vidurkis yra: ', vidurkis);
+console.log('Studentų pažymių vidurkis yra: ',vidurkis);
+
+//kitas variantas:
+
+const marks = [9, 8, 6, 10, 8, 7];
+/*
+const marksSum = marks[0] + marks[1] + marks[2] + marks[3] + marks[4] + marks[5];
+const markAv = marksSum / marks.length;
+const studentMarks = `Studentų pažymių vidurkis yra: ${markAv}.`;
+
+console.log(studentMarks);
+
+variantas su 'Let':
+
+let marksSum = 0;
+marksSum = marksSum + marks[0];
+marksSum = marksSum + marks[1];
+marksSum = marksSum + marks[2];
+marksSum = marksSum + marks[3];
+marksSum = marksSum + marks[4];
+marksSum = marksSum + marks[5];
+*/
+
+let marksSum = 0;
+marksSum += marks[0];
+marksSum += marks[1];
+marksSum += marks[2];
+marksSum += marks[3];
+marksSum += marks[4];
+marksSum += marks[5];
+
+const markAv = marksSum / marks.length;
+const studentMarks = `Studentų pažymių vidurkis yra: ${markAv}.`;
+
+console.log(studentMarks);
+
+
+
+
+
 
 
 const vietos = ['Kaunas,', 'Klaipėda,', 'Skuodas,', 'Vilnius,', 'Šiauliai.'];
 
 console.log('Mūsų aplankytos vietos:', vietos[0], vietos[1], vietos[2], vietos[3], vietos[4]);
 
+// arba:
+
+const places = ['Siaure', 'Menulis', 'Pusiaujas'];
+//const placesStr = `${places[0]}, ${places[1]}, ${places[2]}`;
+//const placesSentence = `Mano aplankytos vetos: ${placesStr}.`;
+
+//let placesStr = '';
+//placesStr = placesStr + places[0];
+//placesStr = placesStr + ', ';
+//placesStr = placesStr + places[1];
+//placesStr = placesStr + ', ';
+//placesStr = placesStr + places[2];
+
+let placesStr = '';
+placesStr += places[0];
+placesStr += ', ';
+placesStr += places[1];
+placesStr += ', ';
+placesStr += places[2];
+
+const placesSentence = `Mano aplankytos vetos: ${placesStr}.`;
+console.log(placesSentence);
+
+let a = 5;
+let x = 7;
+
+//x = x + ++a; // 13
+//x = x++ + a; // 12
+
+//       7    5
+// x = (x++) +a; // 12; x = 8, norint isvengt dviprasmybiu, naudojami skliaustai!
+
+//  7   1+5 
+x = x+ (++a);
+console.log(x);
+
+console.clear();
+const colors = ['Geltona', 'zalia', 'raudona'];
+
+console.log(colors);
+console.log(colors[1]);
+
+colors[1] = 777;
+console.log(colors);
+
+colors[1] = 'ŽALIA';
+console.log(colors);
+
+colors[1] = 'žalia';
+console.log(colors);
+
+const name = 'Chuck';
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
+console.log(name[3]);
+console.log(name[4]);
+
+name[0] = 'H';          //stringams idv poziciju pakeisti negalima!
+console.log(name);
--- kintamieji/number.js
+++ kintamieji/number.js
@@ -9,6 +9,11 @@ Inicijavimo budas:
 - const (default)
 - let (jei kinta informacija)
 - var (niekada)
+
+Matematiniai operatoriai:
++, -, *, /, **, %
++=, -=, *=, **=, %=
+++, --, 
 */
 
 const amzius = 99;
@@ -74,3 +79,120 @@ console.log(liekana);
 
 const liekana2 = 65 % 12;
 console.log (liekana2);
+
+console.clear();
+
+//const wallet = 0;
+//console.log('wallet:', wallet);
+
+let wallet = 0;
+console.log('wallet:', wallet);
+
+wallet = wallet + 5;
+console.log('wallet:', wallet);
+
+wallet += 5;
+wallet += 5;
+wallet += 5;
+console.log('wallet:', wallet);
+
+wallet = wallet - 2;
+console.log('wallet:', wallet);
+
+wallet -= 2;
+wallet -= 2;
+wallet -= 2;
+console.log('wallet:', wallet);
+
+//lygiai taip pat su *, /, 
+
+wallet *= 20;
+console.log('wallet:', wallet);
+
+wallet /= 2;
+console.log('wallet:', wallet);
+
+wallet **= 2;
+console.log('wallet:', wallet);
+
+wallet %= 13;
+console.log('wallet:', wallet);
+
+wallet *= 8;
+console.log('wallet:', wallet);
+
+const skaicius = 256;
+const daliklis = 10;
+const liekana3 = skaicius % daliklis;
+console.log(liekana3);
+
+const sveikaDalis = (skaicius-liekana3) / daliklis;
+console.log(sveikaDalis);
+console.log(sveikaDalis, '*', daliklis, '+', liekana3, '=', skaicius);
+
+console.clear();
+
+//const index = 0;
+//index = index + 1; - const taip neveikia, bet 'let' piktnaudziauti taip pat nerekomenduojama!
+
+let index = 0;
+console.log('index:', index);
+
+index = index + 1;
+console.log('index:', index);
+
+index += 1;
+console.log('index:', index);
+
+index++;
+console.log('index:', index);
+
+++index;
+console.log('index:', index);
+
+let i = 0;
+console.log('i:', i);   // 0
+console.log('i:', i++); // 0 -> 1
+console.log('i:', i);   // 1
+console.log('i:', i++); // 1 -> 2
+console.log('i:', i);   // 2
+
+console.log('==============');
+
+let i2 = 0;
+console.log('i2:', i2);
+console.log('i2:', ++i2);
+console.log('i2:', ++i2);
+console.log('i2:', ++i2);
+console.log('i2:', ++i2);
+
+console.log('==============');
+
+let m = 0
+console.log(m--);
+console.log(m--);
+console.log(m--);
+console.log(m--);
+console.log(m--);
+console.log(m);
+
+console.log('==============');
+
+let m2 = 0
+console.log(--m2);
+console.log(--m2);
+console.log(--m2);
+console.log(--m2);
+console.log(--m2);
+console.log(m2);
+
+console.log('==============');
+
+let klausimas = 10;
+//console.log(++klausimas++); sitox variantas negalimas!
+console.log(++klausimas);
+console.log(++klausimas);
+console.log(++klausimas);
+
+console.log('==============');
+console.clear();
