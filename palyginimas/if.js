747f15c6ef8e2c7649a5f7e974233b195facec24
Darius <dariusandriekus@gmail.com>
2023-09-28 08:28:57 +0300

    If

---
+++ palyginimas/if.js
@@ -0,0 +1,164 @@
+/*
+IF - palyginimo salyga
+
+kodo sablonai:
+if () {} // Ats pateikiamas tik esant atitikimui uzduotai salygai.
+if () {} else {}
+if () {} else if () {}
+if () {} ... else if () {}
+if () {} ... else if () {} ... else {}
+
+Palyginimo operatoriai:
+- visi: >, <, ==, ===, >=, <=, !=, !==
+- naudotini: >, <, ===, >=, <=, !==
+- nenaudotini: ==, !=
+*/
+
+console.clear();
+
+if (4 > 2) {
+    console.log(`4 yra daugiau uz 2`);
+}
+
+if (5 < 0) { 
+console.log(`5 yra maziau uz 0`)
+}
+
+if (true) {
+    console.log(true);
+}
+
+if (false) {
+    console.log(false); // Spausdinama tik esant atitikimui uzduotai salygai.
+}
+
+console.clear();
+
+const a = 7;
+const b = 55;
+
+if (a > b) {
+    console.log(`A yra daugiau uz B`);
+} else {
+    console.log(`A nera daugiau uz B`);
+    //console.log(`A yra maziau arba lygu uz B`);
+}
+
+//===================
+
+const day = 4;
+
+// 1 2 3 4 5 6 7
+// 8 9 10 11 12 13 14
+// 15 16 17 18 19 20 21
+// 22 23 24 25 26 27 28
+
+if (day % 7 == 1) {
+    console.log(`Pirmadienis`);
+} else if (day % 7 == 2) {
+    console.log(`Antradienis`);
+} else if (day % 7 == 3) {
+    console.log(`Treciadienis`);
+} else if (day % 7 == 4) {
+    console.log(`Ketvirtadienis`);
+} else if (day % 7 == 5) {
+    console.log(`Penktadienis`);
+} else if (day % 7 == 6) {
+    console.log(`Sestadienis`);
+} else if (day % 7 == 0) {
+    console.log(`Sekmadienis`);
+} else {
+    console.log(`Tokia diena neegzistuoja!`);
+}
+
+console.clear();
+// Kodo nestinimas
+
+const diena = 99;
+
+if (diena % 7 === 1) {
+    console.log(`Pirmadienis`);
+} else {
+    if (diena % 7 === 2) {
+        console.log('Antradienis');
+    } else {
+        if (diena % 7 === 3) {
+            console.log('Treciadienis');
+        } else { 
+            if (diena % 7 === 'Ketvirtadienis'){
+                console.log('Ketvirtadienis');
+            } else {
+                if (diena % 7 === 5){
+                    console.log('Penktadienis');
+                } else {
+                    if (diena % 7 === 6){
+                        console.log('Sestadienis');
+                    } else {
+                        if (diena % 7 === 7) {
+                            console.log('Sekmadienis');
+                        } else {
+                            console.log('Tokia diena neegzistuoja');
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+/*
+Gali buti:
+- diena / naktis
+- lyja / nelyja
+*/
+
+const parosMetas = 'diena';
+const arLyja = true;
+const temp = -1;
+const kadasilta = 20;
+const arAsSuStriuke = true;
+
+if (parosMetas === 'diena') {
+    if (arLyja) {
+        console.log('Diena, lyja');
+    } else {
+        console.log('Diena, sausa');
+    }
+} else {
+    if (arLyja) {
+        console.log('Naktis, lyja');
+    } else {
+        console.log('Naktis, nelyja');
+        }
+    }
+
+    if (parosMetas === 'diena' && arLyja === true && temp >= kadasilta){
+        console.log('Diena, lyja, silta');        
+    } else if (parosMetas === 'diena' && arLyja === true && temp < kadasilta){
+        console.log('Diena, lyja, salta');        
+    } else if (parosMetas === 'diena' && arLyja === false && temp >= kadasilta) {
+        console.log('Diena, nelyja, silta');    
+    } else if (parosMetas === 'diena' && arLyja === false && temp < kadasilta) {
+        console.log('Diena, nelyja, salta');    
+    } else if (parosMetas === 'naktis' && arLyja === true && temp >= kadasilta) {
+        console.log('Naktis, lyja, silta');
+    } else if (parosMetas === 'naktis' && arLyja === true && temp < kadasilta) {
+        console.log('Naktis, lyja, salta');
+    } else if (parosMetas === 'naktis' && arLyja === false && temp >= kadasilta) {
+        console.log('Naktis, nelyja, silta');
+    } else if (parosMetas === 'naktis' && arLyja === false && temp < kadasilta) {
+        console.log('Naktis, nelyja, salta');
+    }
+
+    console.clear()
+
+    const g = 7;
+    const h = 5;
+
+    if (g > h) {
+        console.log('Daugiau');
+    } else {
+        console.log('Ne daugiau');
+    }
