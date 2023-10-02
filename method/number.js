be2e471e61a0e74cce2fd4ed9368bc6cca8dd1cd
Darius <dariusandriekus@gmail.com>
2023-10-02 18:22:01 +0300

    Method

--- method/number.js
+++ method/number.js
@@ -1,1 +1,130 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
 console.clear();
+
+//KAZKAS.metodas();
+
+console.log(isFinite(5));
+console.log(isFinite(3.14));
+console.log(isFinite(-5));
+console.log(isFinite(-3.14));
+console.log(isFinite(0));
+console.log(isFinite(999999999999999990));
+
+console.log(isFinite(NaN));
+console.log(isFinite(Infinity));
+console.log(isFinite(-Infinity));
+
+const a = 'vienas';
+
+if (isFinite(a)) {
+    console.log('Taip');
+} else {
+    console.log('Ne');
+}
+
+console.clear();
+console.log(Number.isInteger(5));
+console.log(Number.isInteger(1.5));
+console.log(Number.isInteger(-5));
+console.log(Number.isInteger(-1.5));
+console.log(Number.isInteger(NaN));
+console.log(Number.isInteger(Infinity));
+console.log(Number.isInteger(-Infinity));
+
+console.clear();
+
+const b = 10.5;
+
+if (isFinite(b) && Number.isInteger(b)) {
+    console.log('Sveikasis skaicius');
+} else if (isFinite(b) && !Number.isInteger(b)) {
+    console.log('Desimtainis skaicius');        
+} else {
+    console.log('Nenormalus skaicius: NaN arba Infinity');
+
+}
+
+if (isFinite(b)) {
+    if (Number.isInteger(b)) {
+        console.log('Sveikasis skaicius');
+    } else {
+        console.log('Desimtainis skaicius');        
+    }
+} else {
+    console.log('Nenormalus skaicius: NaN arba Infinity');
+}
+console.clear();
+console.log(Number.parseInt(false));
+console.log(Number.parseInt([]));
+console.log(parseInt(''));  //Number nebutinas!
+console.log(parseInt('penki'));
+console.log(parseInt('123'));
+console.log(parseInt('asd123'));
+console.log(parseInt('123asd'));
+console.log(parseInt('123asd456'));//iki pirmos klaidos (NaN)
+console.log(parseInt('123.456')); //tik sveikieji skaiciai.
+console.log(parseInt('Infinity')); //t.b. skaiciai.!
+
+console.log('------------------');
+console.log(parseFloat('123'));
+console.log(parseFloat(123)); //ir stringas ir skaicius
+console.log(parseFloat(''));
+console.log(parseFloat('789'));
+console.log(parseFloat('789.101'));
+console.log(parseFloat('Infinity'));
+console.log(parseFloat('789.101asd'));
+console.log(parseFloat('-asd789.101'));
+
+console.clear();
+console.log(isNaN('asd78'));
+console.log(isNaN('5'));
+console.log(isNaN(5));
+console.log(isNaN(NaN));
+
+console.clear();
+
+const kaina = 3.14;
+console.log(kaina, '3.14', kaina.toFixed(0));
+console.log(kaina, '3.14', kaina.toFixed(1));
+console.log(kaina, '3.14', kaina.toFixed(2));
+console.log(kaina, '3.14', kaina.toFixed(3));
+console.log(kaina, '3.14', kaina.toFixed(4));
+console.log(kaina, '3.14', kaina.toFixed(5));
+
+const keistasSkaicius = 0.1 + 0.2;
+console.log(keistasSkaicius);
+console.log(keistasSkaicius.toFixed(10));
+
+const keistasSkaicius2 = keistasSkaicius.toFixed(3);
+console.log(keistasSkaicius2);
+
+const keistasSkaicius3 = parseInt(keistasSkaicius2);
+console.log(keistasSkaicius3);
+
+const keistasSkaicius4 = parseFloat(keistasSkaicius2);
+console.log(keistasSkaicius4);
+
+const keistasSkaicius5 = +'1.23'; //parseFloat ir + yra kaip ir tas pats.
+console.log(keistasSkaicius5);
+
+const keistasSkaicius6 = +keistasSkaicius2;
+console.log(keistasSkaicius6);
+
+const c = 6.9999999999999999;
+console.log(c);
+console.log(c.toFixed(5));
+
+const d = 5.6789;
+console.log(d.toFixed(6));
+console.log(d.toFixed(5));
+console.log(d.toFixed(4));
+console.log(d.toFixed(3));
+
+console.log('===================')
+
+const e = 5.6784;
+console.log(e.toFixed(6));
+console.log(e.toFixed(5));
+console.log(e.toFixed(4));
+console.log(e.toFixed(3));
+
---
+++ method/string.js
@@ -0,0 +1,139 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+
+console.clear();
+
+console.log('labas'.length);
+console.log('     '.length);
+console.log('\n\'\\'.length);
+
+console.log('labas'[0], 'labas'.at(0));
+console.log('labas'[1], 'labas'.at(1));
+console.log('labas'[2], 'labas'.at(2));
+console.log('labas'[3], 'labas'.at(3));
+console.log('labas'[4], 'labas'.at(4));
+console.log('labas'[-1], 'labas'.at(-1));
+console.log('labas'[-2], 'labas'.at(-2));
+console.log('labas'[-3], 'labas'.at(-3));
+console.log('labas'[-4], 'labas'.at(-4));
+console.log('labas'[-5], 'labas'.at(-5));
+console.log('labas'[-6], 'labas'.at(-6));
+
+const hi = 'labas';
+console.log(hi.at(-2), hi[hi.length-2]);
+
+console.clear();
+
+console.log('a'.charCodeAt(0));
+console.log('b'.charCodeAt(0));
+console.log('labas'.charCodeAt(0));
+console.log('labas'.charCodeAt(1));
+console.log('labas'.charCodeAt(4));
+
+console.clear();
+const str1 = 'Hello';
+const str2 = 'World';
+
+console.log(str1.concat(' ', str2));
+
+let str3 = 'Hello';
+const str4 = 'World';
+
+str3 += ' ';
+str3 += str4;
+console.log(str3);
+
+let str5 = 'Hello';
+str5 += ' ' + str4;
+console.log(str5);
+
+console.clear();
+
+const abc = ['a', 'b', 'c', 'd', 'e', 'f'];
+const start = 'Abecele: ';
+
+let a1 = '';
+a1 += start;
+a1 += abc[0];
+a1 += abc[1];
+a1 += abc[2];
+a1 += abc[3];
+a1 += abc[4];
+a1 += abc[5];
+console.log(a1);
+
+let a2 = start + abc[0] + abc[1] + abc[2] + abc[3] + abc[4] + abc[5];
+console.log(a2);
+
+let a3 = start;
+for (let i = 0; i < abc.length; i++) {
+    a3 += abc[i];
+}
+
+const a4 = start.concat(...abc); // '...' isvardija visas masyvo reiksmes!
+console.log(a4)
+
+console.clear();
+
+console.log('labas rytas'.includes('a'));
+console.log('labas rytas'.includes('as'));
+console.log('labas rytas'.includes('A'));
+console.log('labas rytas'.includes(' '));
+console.log('labas rytas'.includes('ab'));
+console.log('labas rytas'.includes('rytas'));
+console.log('labas rytas'.includes('labas')); // .includes - ar yra?
+
+console.log('labas rytas'.indexOf('a'));
+console.log('labas rytas'.indexOf('as'));
+console.log('labas rytas'.indexOf('A'));
+console.log('labas rytas'.indexOf(' '));
+console.log('labas rytas'.indexOf('ab'));
+console.log('labas rytas'.indexOf('rytas'));
+console.log('labas rytas'.indexOf('labas')); // .indexOf - ar yra ir kur?
+
+console.log('===================')
+
+console.log('labas rytas'.indexOf('a'));
+console.log('labas rytas'.indexOf('a', 0));
+console.log('labas rytas'.indexOf('a', 1));
+console.log('labas rytas'.indexOf('a', 2));
+console.log('labas rytas'.indexOf('a', 4));
+console.log('labas rytas'.indexOf('a', 10));
+
+function kiek(tekstas, raide) {
+    return 0;
+}
+console.log(kiek('labas', 'a'));
+
+console.clear();
+console.log('a'.repeat(6));
+console.log('labas '.repeat(6));
+
+console.clear();
+const r1 = 'labas';
+const r2 = r1.replace('a', 'x');
+
+console.log(r1, r2);
+
+const r3 = 'Labas rytas';
+const r4 = r3.replace('rytas', 'vakaras');
+console.log(r3, r4);
+
+const r5 = r3.replace('xxx', 'yyy');
+console.log(r3, r5);
+
+const r6 = 'labas';
+const r7 = r6.replaceAll('a', '_');
+console.log(r6, r7);
+
+let r8 = 'vasara';
+r8 = r8.replace('a', 'o');
+console.log(r8);
+
+r8 = r8.replace('a', 'o');
+console.log(r8);
+
+r8 = r8.replace('a', 'o');
+console.log(r8);
+
+
+
